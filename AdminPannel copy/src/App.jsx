97344cbe2dbdabc1e09153b20e796b0ea2fedb66
teambuildnex-star <teambuildnex@gmail.com>
2026-02-18 14:35:04 +0530

    update css only

--- AdminPannel copy/src/App.jsx
+++ AdminPannel copy/src/App.jsx
@@ -1,52 +1,62 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import Navebar from './components/Navebar.jsx'
-import Sidebar from './components/Sidebar.jsx'
-import { Routes,Route } from 'react-router-dom'
-import Add from './pages/Add.jsx'
-import List from './pages/List.jsx'
-import Update from './pages/Update.jsx'
-import Order from './pages/Order.jsx'
-import Login from './components/Login.jsx'
-import { ToastContainer } from 'react-toastify'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Navebar from "./components/Navebar.jsx";
+import Sidebar from "./components/Sidebar.jsx";
+import { Routes, Route } from "react-router-dom";
+import Add from "./pages/Add.jsx";
+import List from "./pages/List.jsx";
+import Update from "./pages/Update.jsx";
+import Order from "./pages/Order.jsx";
+import Login from "./components/Login.jsx";
+import { ToastContainer } from "react-toastify";
 
 function App() {
-  const [token, settoken] = useState(localStorage.getItem("token")?localStorage.getItem("token"):"")
-  useEffect(()=>{
-    localStorage.setItem("token",token)
-  },[token])
+  const [token, settoken] = useState(
+    localStorage.getItem("token") || ""
+  );
+
+  useEffect(() => {
+    localStorage.setItem("token", token);
+  }, [token]);
 
   return (
-    
-    <div className="min-h-screen bg-gray-50">
-      <ToastContainer/>
-      { token==="" ? 
-      <Login settoken={settoken}/>
-      : <>
-       {/* Navbar at the top */}
-      <Navebar settoken={settoken} />
-      <hr/>
+    <div className="min-h-screen bg-gray-100">
 
-      <div className="flex w-full">
-        {/* Sidebar on the left */}
-        <Sidebar  />
-        <div>
-          <Routes>
-            <Route  path='/add' element={<Add token={token}/>}/>
-            <Route  path='/products' element={<List token={token}/>}/>
-            <Route  path='/update' element={<Update token={token}/>}/>
-            <Route  path='/order' element={<Order token={token}/>}/>
-            
-          </Routes>
-        </div>
+      <ToastContainer />
 
-        
-      </div>
-      </>}
-      
+      {token === "" ? (
+        <Login settoken={settoken} />
+      ) : (
+        <>
+          {/* Navbar */}
+          <Navebar settoken={settoken} />
+          <hr />
+
+          {/* Main Layout */}
+          <div className="flex flex-col md:flex-row w-full">
+
+            {/* Sidebar */}
+            <div className="w-full md:w-64 bg-white shadow-md">
+              <Sidebar />
+            </div>
+
+            {/* Page Content */}
+            <div className="flex-1 p-4 sm:p-6 lg:p-8">
+              <div className="bg-white rounded-lg shadow-sm p-4 sm:p-6 min-h-[80vh]">
+                <Routes>
+                  <Route path="/add" element={<Add token={token} />} />
+                  <Route path="/products" element={<List token={token} />} />
+                  <Route path="/update" element={<Update token={token} />} />
+                  <Route path="/order" element={<Order token={token} />} />
+                </Routes>
+              </div>
+            </div>
+
+          </div>
+        </>
+      )}
     </div>
-    
-  )
+  );
 }
 
-export default App
+export default App;
