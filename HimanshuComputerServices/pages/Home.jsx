20d23a5b537425826915489f27f3b9457e3c78a1
teambuildnex-star <teambuildnex@gmail.com>
2026-02-18 13:31:42 +0530

    revrse

--- HimanshuComputerServices/pages/Home.jsx
+++ HimanshuComputerServices/pages/Home.jsx
@@ -1,27 +1,20 @@
-import React from "react";
-import LatestCollation from "../components/LatestCollation.jsx";
-import Collection from "./Collection.jsx";
+import React from 'react'
+
+import LatestCollation from '../components/LatestCollation.jsx'
+import Collection from './Collection'
+import { useLocation } from 'react-router-dom'
+
+
 
 function Home() {
-  return (
-    <main>
 
-      {/* Main SEO Heading */}
-      <section>
-        <h1>
-          Professional Website Development & IT Solutions
-        </h1>
-        <p>
-          Himanshu Computer Services helps businesses grow online with modern,
-          fast, and responsive websites, SEO services, and digital solutions.
-        </p>
-      </section>
-
-      <Collection />
-      <LatestCollation />
-
-    </main>
-  );
+    return (
+        <div>
+       <Collection/>
+       <LatestCollation/>
+       
+       </div>
+    )
 }
 
-export default Home;
+export default Home
